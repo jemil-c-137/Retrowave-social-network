4b36a092bb5bb2b11d72115009ff0c6c24f7d051
Dzhemil Suleimanov <jemil1931@gmail.com>
2020-07-23 00:04:54 +0300

    lesson 55 component did mount

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -5,14 +5,15 @@ import * as axios from 'axios'
 
 class Users extends React.Component {
 
-  constructor(props) {
-    super(props);
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(response => {
-        this.props.setUsers(response.data.items)
-      })
-  }
+  componentDidMount() {
+    if(this.props.users.length === 0) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+          this.props.setUsers(response.data.items)
+        })
+    }
 
+  }
 
   render() {
 
