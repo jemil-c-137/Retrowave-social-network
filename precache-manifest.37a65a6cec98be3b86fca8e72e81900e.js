0009fb43dfcf9b00964564b7b7d3c4c78087e7ed
Jemil <jemil1931@gmail.com>
2021-01-29 23:41:03 +0300

    Updates

---
+++ precache-manifest.37a65a6cec98be3b86fca8e72e81900e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9da91c902d255bce4e5654815b70ac7",
+    "url": "/jemil-c-137/Retrowave-social-network/index.html"
+  },
+  {
+    "revision": "b20c4fe51b4bf1ace010",
+    "url": "/jemil-c-137/Retrowave-social-network/static/css/3.37a22cf9.chunk.css"
+  },
+  {
+    "revision": "7114cfda012e020553c2",
+    "url": "/jemil-c-137/Retrowave-social-network/static/css/4.ec4635d7.chunk.css"
+  },
+  {
+    "revision": "66caad35eaa36b7376ca",
+    "url": "/jemil-c-137/Retrowave-social-network/static/css/main.584ad0b7.chunk.css"
+  },
+  {
+    "revision": "b37c792a00b8b51fc40f",
+    "url": "/jemil-c-137/Retrowave-social-network/static/js/2.61cb75f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jemil-c-137/Retrowave-social-network/static/js/2.61cb75f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20c4fe51b4bf1ace010",
+    "url": "/jemil-c-137/Retrowave-social-network/static/js/3.7a8f4013.chunk.js"
+  },
+  {
+    "revision": "7114cfda012e020553c2",
+    "url": "/jemil-c-137/Retrowave-social-network/static/js/4.09708bb6.chunk.js"
+  },
+  {
+    "revision": "66caad35eaa36b7376ca",
+    "url": "/jemil-c-137/Retrowave-social-network/static/js/main.9d3fd980.chunk.js"
+  },
+  {
+    "revision": "8159f0f9cc393fc2a72e",
+    "url": "/jemil-c-137/Retrowave-social-network/static/js/runtime-main.a5d43ee3.js"
+  }
+]);
